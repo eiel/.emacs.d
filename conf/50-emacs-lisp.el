95948ae740d05fcf3c19aade714ab5849c1e3a13
Tomohiko Himura <eiel.hal@gmail.com>
2012-04-13 17:04:03 +0900

    change setting eldoc

--- conf/50-emacs-lisp.el
+++ conf/50-emacs-lisp.el
@@ -14,7 +14,8 @@
 
 ;; (install-elisp-from-emacswiki "eldoc-extension.el")
 (require 'eldoc-extension)
-(setq eldoc-idle-delay 1.0)
+(setq eldoc-idle-delay 0.2)
+(setq eldoc-minor-mode-string "")
 (setq eldoc-echo-area-use-multiline-p t)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
